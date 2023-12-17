3699a605dd2846e0e53c16ab4f0eb0f239dd0524
Merve Balci <mervebalci090@gmail.com>
2023-12-16 23:00:29 -0400

    Passing each button's value when it's clicked

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 export default function App() {
+  const[result, setResult] = useState('0')
+  function handleClick(value) {
+    console.log(value)
+  }
   return (
     <div className="App">
       <h1>CALCULATOR</h1>
       <div>
-        <button className="square">AC</button>
-        <button className="square">+/-</button>
-        <button className="square">%</button>
-        <button className="square">÷</button>
+        <button className="square" id="AC" onClick={() => handleClick('AC')}>AC</button>
+        <button className="square" id="+/-" onClick={() => handleClick('+/-')}>+/-</button>
+        <button className="square" id="%" onClick={() => handleClick('%')}>%</button>
+        <button className="square" id="÷" onClick={() => handleClick('÷')}>÷</button>
       </div>
       <div>
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
-        <button className="square">x</button>
+        <button className="square" id="7" onClick={() => handleClick('7')}>7</button>
+        <button className="square" id="8" onClick={() => handleClick('8')}>8</button>
+        <button className="square" id="9" onClick={() => handleClick('9')}>9</button>
+        <button className="square" id="x" onClick={() => handleClick('x')}>x</button>
       </div>
       <div>
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
-        <button className="square">-</button>
+        <button className="square" id="4" onClick={() => handleClick('4')}>4</button>
+        <button className="square" id="5" onClick={() => handleClick('5')}>5</button>
+        <button className="square" id="6" onClick={() => handleClick('6')}>6</button>
+        <button className="square" id="-" onClick={() => handleClick('-')}>-</button>
       </div>
       <div>
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
-        <button className="square">+</button>
+        <button className="square" id="1" onClick={() => handleClick('1')}>1</button>
+        <button className="square" id="2" onClick={() => handleClick('2')}>2</button>
+        <button className="square" id="3" onClick={() => handleClick('3')}>3</button>
+        <button className="square" id="+" onClick={() => handleClick('+')}>+</button>
       </div>
       <div>
-        <button className="rectangle">0</button>
-        <button className="square">.</button>
-        <button className="square">=</button>
+        <button className="rectangle" id="0" onClick={() => handleClick('0')}>0</button>
+        <button className="square" id="." onClick={() => handleClick('.')}>.</button>
+        <button className="square" id="=" onClick={() => handleClick('=')}>=</button>
       </div>
-      <h2></h2>
+      <h2>{result}</h2>
     </div>
   );
 }
