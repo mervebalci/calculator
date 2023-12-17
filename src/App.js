fd991c86ceaba4eaa029631262e58e55d49628bd
Merve Balci <mervebalci090@gmail.com>
2023-12-16 20:59:21 -0400

    Adding the basic html and css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CALCULATOR</h1>
+      <div>
+        <button className="square">AC</button>
+        <button className="square">+/-</button>
+        <button className="square">%</button>
+        <button className="square">÷</button>
+      </div>
+      <div>
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+        <button className="square">x</button>
+      </div>
+      <div>
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+        <button className="square">-</button>
+      </div>
+      <div>
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+        <button className="square">+</button>
+      </div>
+      <div>
+        <button className="rectangle">0</button>
+        <button className="square">.</button>
+        <button className="square">=</button>
+      </div>
+      <h2></h2>
     </div>
   );
 }
-
-export default App;
